e40b0e318779bc25f515acb0cddda720194a83f8
BPsaren <bishnupadasaren78@gmail.com>
2024-03-06 04:49:49 +0530

    948. Bag of Tokens

---
+++ Day-3/day_3.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+    int n= tokens.size();
+    int score= 0;
+    int i=0;
+    int j=n-1;
+    int maxscore=0;
+
+    sort(tokens.begin(),tokens.end());
+    while(i<=j){
+       if(power>=tokens[i]){
+       power-=tokens[i];
+       score+=1;
+       i++;
+       maxscore=max(maxscore,score);
+       }else if(score>=1){
+           power+=tokens[j];
+           score-=1;
+           j--;
+       }else{
+           return maxscore;
+       }
+    }
+    return maxscore;
+    }
+};
